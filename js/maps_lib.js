9e5e7836dd3fd6932f455b47f05dd449debb5353
OHAMVETS <kendra@ohamvets.org>
2017-03-21 10:09:08 -0400

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -9,7 +9,7 @@
         this.searchRadius = options.searchRadius || 805; //in meters ~ 1/2 mile
 
         // the encrypted Table ID of your Fusion Table (found under File => About)
-        this.fusionTableId = options.fusionTableId || "",
+        this.fusionTableId = options.fusionTableId || "1mzS8JKR06-DRYdE3cqvkyzAmpXFFyr_80YPkTk1s",
 
         // Found at https://console.developers.google.com/
         // Important! this key is for demonstration purposes. please register your own.
@@ -18,7 +18,7 @@
         // name of the location column in your Fusion Table.
         // NOTE: if your location column name has spaces in it, surround it with single quotes
         // example: locationColumn:     "'my location'",
-        this.locationColumn = options.locationColumn || "geometry";
+        this.locationColumn = options.locationColumn || "Location";
         
         // appends to all address searches if not present
         this.locationScope = options.locationScope || "";
@@ -163,7 +163,7 @@
         self.whereClause = self.locationColumn + " not equal to ''";
         
         //-----custom filters-----
-       var type_column = "'Type of Post'";
+       var type_column = "'Post'";
 var tempWhereClause = [];
 if ( $("#cbType1").is(':checked')) tempWhereClause.push("Paper Post");
 if ( $("#cbType2").is(':checked')) tempWhereClause.push("Post Home");
