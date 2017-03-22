3001371d472a054a71b7c580b7718e6057ccc42e
OHAMVETS <kendra@ohamvets.org>
2017-03-22 09:09:57 -0400

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -163,12 +163,12 @@
         self.whereClause = self.locationColumn + " not equal to ''";
         
         //-----custom filters-----
-       var type_column = "'Post'";
-var tempWhereClause = [];
-if ( $("#cbType1").is(':checked')) tempWhereClause.push("'Post With Canteen");
-if ( $("#cbType2").is(':checked')) tempWhereClause.push("Post Home");
-if ( $("#cbType3").is(':checked')) tempWhereClause.push("Paper Post");
-self.whereClause += " AND " + type_column + " IN ('" + tempWhereClause.join("','") + "')";
+var type_column = "'type'";
+var searchType = type_column + " IN (-1,";
+if ( $("#cbType1").is(':checked')) searchType += "3,";
+if ( $("#cbType2").is(':checked')) searchType += "2,";
+if ( $("#cbType3").is(':checked')) searchType += "1,";
+self.whereClause += " AND " + searchType.slice(0, searchType.length - 1) + ")";
         //-----end of custom filters-----
 
         self.getgeoCondition(address, function (geoCondition) {
